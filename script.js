e899b458ebe579294cb65c0ee8674fe6548bc7f6
Stefano Mercadante <stefmercadante@gmail.com>
2022-06-29 12:28:16 +0200

    commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let names = [
+  "Stefano",
+  "Nicola",
+  "Rondi",
+  "Mirko",
+  "Giosue",
+  "Gio",
+  "Pigna",
+  "Gommo"
+]
+
+const createLeaderboard = () => {
+  const _names = localStorage.getItem("names");
+  if (_names.length) {
+    names = JSON.parse(_names);
+  }
+  else {
+    names.sort(() => Math.random() - 0.5)
+    localStorage.setItem("names", JSON.stringify(names));
+  }
+  const leaderboardEl = document.querySelector(".leaderboard");
+  for (const name of names) {
+    const liEl = document.createElement("li")
+    liEl.innerHTML = name;
+    leaderboardEl.appendChild(liEl)
+  }
+}
+
+createLeaderboard();
